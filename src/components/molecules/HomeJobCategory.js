61586e16b412bbef95baef9670ee540bbf82e264
Okereke Chinweotito <okerekechinweotito@gmail.com>
2022-06-02 00:17:55 +0100

    add browse talent by category

---
+++ src/components/molecules/HomeJobCategory.js
@@ -0,0 +1,95 @@
+import { Box, Container, Typography } from "@mui/material";
+import React from "react";
+import CategoryCard from "../atoms/CategoryCard";
+
+function HomeJobCategory() {
+  return (
+    <>
+      <Container>
+        <Typography
+          variant="h3"
+          sx={{
+            mt: "2em",
+            fontFamily: "Merriweather",
+            fontWeight: "bold",
+          }}
+        >
+          Browse talent by category
+        </Typography>
+        <Typography variant="h6" color="text.secondary" sx={{ mb: "2em" }}>
+          Looking for work ? {""}
+          <Typography
+            variant="span"
+            color="#14A800"
+            sx={{
+              "&:hover": {
+                color: "darkgreen",
+                cursor: "pointer",
+                textDecoration: "underline",
+              },
+            }}
+          >
+            Browse Jobs
+          </Typography>
+        </Typography>
+      </Container>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+        }}
+      >
+        <CategoryCard
+          categoryName={"Development & IT"}
+          categoryRating={` 4.85/5`}
+          jobCount={`1,853 jobs`}
+        />
+
+        <CategoryCard
+          categoryName={"Design and Creative"}
+          categoryRating={` 4.91/5`}
+          jobCount={`995 jobs`}
+        />
+        <CategoryCard
+          categoryName={"Sales & Marketing"}
+          categoryRating={` 4.77/5`}
+          jobCount={`392 jobs`}
+        />
+        <CategoryCard
+          categoryName={"Writing & Translation"}
+          categoryRating={` 4.11/5`}
+          jobCount={`505 jobs`}
+        />
+        <CategoryCard
+          categoryName={"Admin & Customer Support"}
+          categoryRating={` 4.32/5`}
+          jobCount={`508 jobs`}
+        />
+        <CategoryCard
+          categoryName={"Finance & Accounting"}
+          categoryRating={` 4.91/5`}
+          jobCount={`214 jobs`}
+        />
+        <CategoryCard
+          categoryName={"Engineering & Architecture"}
+          categoryRating={` 4.56/5`}
+          jobCount={`133 jobs`}
+        />
+        <CategoryCard
+          categoryName={"Legal"}
+          categoryRating={` 4.79/5`}
+          jobCount={`650 jobs`}
+        />
+        <CategoryCard
+          categoryName={"Music & Literature"}
+          categoryRating={` 4.39/5`}
+          jobCount={`1,232 jobs`}
+        />
+      </Container>
+    </>
+  );
+}
+
+export default HomeJobCategory;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HomeHero from "../components/molecules/HomeHero";
 import HomeStats from "../components/molecules/HomeStats";
+import HomeJobCategory from "../components/molecules/HomeJobCategory";
 
 export default function Home() {
   return (
     <>
       <HomeHero />
+      <HomeJobCategory />
       <HomeStats />
     </>
   );
