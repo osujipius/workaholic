bab04b48b5aa2aa573f4591b57554b7488b197f3
Okereke Chinweotito <okerekechinweotito@gmail.com>
2022-06-08 18:51:15 +0100

    quack footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/molecules/Header.js";
 import Home from "./pages/Home.js";
-import Footer from "./components/molecules/Footer";
+import Footer from "./components/molecules/footer/Footer";
 
 export default function App() {
   return (
